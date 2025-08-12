b4e17fe730edd3f7792f0c1fd4cf74a8d6bf2da0
matriz2109 <sheilacindyguarachi@gmail.com>
2025-08-12 07:28:56 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,40 @@
+
+notas=[8, 5, 3, 4]
+ 
+ soma= [0]
+//soma = 0; 
+//soma = 6;
+//soma = 16;
+//soma = 26;
+//soma = 8;
+for(nota of notas){
+    console.log("volar e: ", nota," volar da soma :", soma)
+    soma += nota
+}
+
+console.log(soma/4)
+
+
+/*
+Cria uma lista das coisa que voce gosta 
+e mostre  todos os itens com o comando for
+atraves do console.log.
+*/ 
+
+
+
+
+doces=[8, 4, 6, 4]
+ 
+ soma= [0]
+//soma = 0; 
+//soma = 6;
+//soma = 16;
+//soma = 26;
+//soma = 8;
+for(doce of doces){
+    console.log("volar e: ", doce," volar da soma :", soma)
+    soma += doce
+}  
+
+console.log(soma/4 )    
